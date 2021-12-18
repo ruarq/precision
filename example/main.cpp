fd12df05773d1fc0b504be319bb8e80904963c98
ruarq <anton@green-pr.org>
2021-12-18 16:23:12 +0100

    removed leftover code

--- example/main.cpp
+++ example/main.cpp
@@ -3,9 +3,6 @@
 
 #include "precision/precision.hpp"
 
-std::vector<int> nums;
-constexpr static auto nums_size = 1'000'000;
-
 auto benchmarks = {
 	precision::benchmark{
 		"create-random-array",
@@ -58,10 +55,5 @@ auto benchmarks = {
 
 int main(int argc, char **argv)
 {
-	for (int i = 0; i < nums_size; ++i)
-	{
-		nums.push_back(std::rand() % 1'000);
-	}
-
 	return precision::main(argc, argv, benchmarks);
 }
