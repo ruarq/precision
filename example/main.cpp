265b49a545aba3e69774b0f940457f939c53e8a0
ruarq <anton@green-pr.org>
2022-01-02 22:14:13 +0100

    added list-sort benchmark

--- example/main.cpp
+++ example/main.cpp
@@ -1,9 +1,13 @@
 #include <vector>
+#include <list>
 #include <algorithm>
 
 #include <precision/precision.hpp>
 
 auto benchmarks = {
+	/**
+	 * vector-sort
+	 */
 	precision::benchmark {
 		"vector-sort",
 		[](auto &ctx) {
@@ -22,7 +26,30 @@ auto benchmarks = {
 				std::sort(v.begin(), v.end());
 			}
 		}
-	}.time(std::chrono::seconds(10))
+	},
+
+	/**
+	 * list-sort
+	 */
+	precision::benchmark {
+		"list-sort",
+		[](auto &ctx) {
+			std::srand(std::time(nullptr));
+
+			while (ctx.running())
+			{
+				ctx.pause();
+				std::list<int> l;
+				for (int i = 0; i < 10000; ++i)
+				{
+					l.push_back(std::rand() % 1000);
+				}
+				ctx.resume();
+
+				l.sort();
+			}
+		}
+	}
 };
 
 auto main() -> int
